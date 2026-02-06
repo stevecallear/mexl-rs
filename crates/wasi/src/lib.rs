529e3625ddbcf08a15150a724b3de83a48b606ee
Steve Callear <scallear@gmail.com>
2026-02-06 14:43:02 +0000

    feat: initial wasi implementation

---
+++ crates/wasi/src/lib.rs
@@ -0,0 +1,73 @@
+use std::{
+    alloc::{self, Layout},
+    slice, str,
+};
+
+use crate::module::{CompileResponse, RunResponse};
+
+mod module;
+
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn compile(ptr: *mut u8, len: usize) -> u64 {
+    unsafe {
+        let expr_bytes = slice::from_raw_parts(ptr, len);
+        let expr_str = match str::from_utf8(expr_bytes) {
+            Ok(s) => s,
+            Err(e) => return pack_string(CompileResponse::error(e.to_string()).to_json()),
+        };
+
+        let response = module::compile(expr_str);
+        pack_string(response.to_json())
+    }
+}
+
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn run(program_id: u64, env_ptr: *mut u8, env_len: usize) -> u64 {
+    unsafe {
+        let env_bytes = slice::from_raw_parts(env_ptr, env_len);
+        let env_str = match str::from_utf8(env_bytes) {
+            Ok(s) => s,
+            Err(e) => return pack_string(RunResponse::error(e.to_string()).to_json()),
+        };
+
+        let response = module::run(program_id, env_str);
+        pack_string(response.to_json())
+    }
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn alloc(size: usize) -> *mut u8 {
+    let layout = get_u8_layout(size);
+    unsafe {
+        let ptr = alloc::alloc(layout);
+        if ptr.is_null() {
+            alloc::handle_alloc_error(layout);
+        }
+        ptr
+    }
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn dealloc(ptr: *mut u8, size: usize) {
+    let layout = get_u8_layout(size);
+    unsafe {
+        alloc::dealloc(ptr, layout);
+    }
+}
+
+fn get_u8_layout(size: usize) -> std::alloc::Layout {
+    Layout::array::<u8>(size).unwrap()
+}
+
+fn pack_string(mut s: String) -> u64 {
+    s.shrink_to_fit();
+
+    let mut output = s.into_bytes();
+    output.shrink_to_fit();
+
+    let ptr = output.as_mut_ptr();
+    let len = output.len();
+    std::mem::forget(output);
+
+    ((len as u64) << 32) | (ptr as u64)
+}
